406ffb7c3c9483077bcdeee56b97c82b34006216
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-07-05 23:56:09 +0200

    Add files via upload

---
+++ ParamsExample/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ParamsExample
+{
+    public class Program
+    {
+        public static void printAll(params string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+        static void Main(string[] args)
+        {
+            printAll("Andrzej", "Bartek", "Czesław", "Dariusz", "Elżbieta");
+        }
+    }
+}
